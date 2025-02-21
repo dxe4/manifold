cad8012328189db7b1d65511fe4faa11070efe03
charalmapos papaloizou <harry@e2e4.email>
2025-02-21 08:45:25 +0000

    add pyi file

--- manifold/__init__.py
+++ manifold/__init__.py
@@ -1,5 +1,4 @@
 from manifold_rs import (
-    add_numbers,
     miller_rabin_bool,
     miller_rabin_bool_multiple,
     power_of_two_exponent_10n_py,
@@ -7,7 +6,6 @@ from manifold_rs import (
 )
 
 __all__ = [
-    "add_numbers",
     "miller_rabin_bool",
     "miller_rabin_bool_multiple",
     "power_of_two_exponent_10n_py",
--- src/lib.rs
+++ src/lib.rs
@@ -47,38 +47,6 @@ fn collatz_sequence(a: &PyAny) -> Collatz {
 }
 
 #[pyfunction]
-fn add_numbers(a: &PyAny, b: &PyAny) -> PyResult<PyObject> {
-    fn to_rug_integer(obj: &PyAny) -> PyResult<Integer> {
-        if let Ok(int_val) = obj.extract::<i64>() {
-            Ok(Integer::from(int_val))
-        } else if let Ok(str_val) = obj.extract::<String>() {
-            Integer::from_str(&str_val).map_err(|_| {
-                pyo3::exceptions::PyValueError::new_err("Invalid string for integer conversion.")
-            })
-        } else {
-            Err(pyo3::exceptions::PyTypeError::new_err(
-                "Argument must be an integer or a string.",
-            ))
-        }
-    }
-
-    let num_a = to_rug_integer(a)?;
-    let num_b = to_rug_integer(b)?;
-
-    let sum = num_a + num_b;
-
-    Python::with_gil(|py| {
-        if let Some(i64_val) = sum.to_i64() {
-            Ok(i64_val.into_py(py))
-        } else if let Some(u64_val) = sum.to_u64() {
-            Ok(u64_val.into_py(py))
-        } else {
-            Ok(sum.to_string().into_py(py))
-        }
-    })
-}
-
-#[pyfunction]
 fn power_of_two_exponent_10n_py(start: usize, end: usize) -> PyResult<Vec<String>> {
     let config = NumberConfig {
         start: start,
@@ -91,7 +59,6 @@ fn power_of_two_exponent_10n_py(start: usize, end: usize) -> PyResult<Vec<String
 #[pymodule]
 fn manifold_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(miller_rabin_bool, m)?)?;
-    m.add_function(wrap_pyfunction!(add_numbers, m)?)?;
     m.add_function(wrap_pyfunction!(miller_rabin_bool_multiple, m)?)?;
     m.add_function(wrap_pyfunction!(power_of_two_exponent_10n_py, m)?)?;
     m.add_function(wrap_pyfunction!(collatz_sequence, m)?)?;
